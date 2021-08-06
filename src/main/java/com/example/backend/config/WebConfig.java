edc5a6bbeb8c3940b5ec01e620d420dfdd6b26a9
KimYujeong <serena35@example.com>
2021-08-06 16:21:33 +0900

    2021-08-06

---
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.example.backend.controller"}) // 하위 패키지 내부의 컴포넌트 스캔
+public class WebConfig implements WebMvcConfigurer{
+}
