85434047f6e7d42e3026c06260e008fee6e37ebd
KimYujeong <serena35@example.com>
2021-08-06 16:18:07 +0900

    2021-08-06

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,20 @@
 package com.example.backend;
 
+import com.example.backend.config.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.backend")
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+		//ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 	}
 
 }
